4eef88a496c5b3de7e8f308619fab49955a93096
Andy Wu <andywu0913@yahoo.com.tw>
2020-04-23 15:32:25 +0800

    Minor changes.

--- play_car_racing_with_keyboard.py
+++ play_car_racing_with_keyboard.py
@@ -10,99 +10,99 @@ gas            = 0 # init to 0
 break_system   = 0 # init to 0
 
 def key_press(key, mod):
-	global is_pressed_left
-	global is_pressed_right
-	global is_pressed_space
-	global is_pressed_shift
-	global is_pressed_esc
+    global is_pressed_left
+    global is_pressed_right
+    global is_pressed_space
+    global is_pressed_shift
+    global is_pressed_esc
 
-	if key == 65361:
-		is_pressed_left = True
-	if key == 65363:
-		is_pressed_right = True
-	if key == 32:
-		is_pressed_space = True
-	if key == 65505:
-		is_pressed_shift = True
-	if key == 65307:
-		is_pressed_esc = True
+    if key == 65361:
+        is_pressed_left = True
+    if key == 65363:
+        is_pressed_right = True
+    if key == 32:
+        is_pressed_space = True
+    if key == 65505:
+        is_pressed_shift = True
+    if key == 65307:
+        is_pressed_esc = True
 
 def key_release(key, mod):
-	global is_pressed_left
-	global is_pressed_right
-	global is_pressed_space
-	global is_pressed_shift
+    global is_pressed_left
+    global is_pressed_right
+    global is_pressed_space
+    global is_pressed_shift
 
-	if key == 65361:
-		is_pressed_left = False
-	if key == 65363:
-		is_pressed_right = False
-	if key == 32:
-		is_pressed_space = False
-	if key == 65505:
-		is_pressed_shift = False
+    if key == 65361:
+        is_pressed_left = False
+    if key == 65363:
+        is_pressed_right = False
+    if key == 32:
+        is_pressed_space = False
+    if key == 65505:
+        is_pressed_shift = False
 
 def update_action():
-	global steering_wheel
-	global gas
-	global break_system
+    global steering_wheel
+    global gas
+    global break_system
 
-	if is_pressed_left ^ is_pressed_right:
-		if is_pressed_left:
-			if steering_wheel > -1:
-				steering_wheel -= 0.1
-			else:
-				steering_wheel = -1
-		if is_pressed_right:
-			if steering_wheel < 1:
-				steering_wheel += 0.1
-			else:
-				steering_wheel = 1
-	else:
-		if abs(steering_wheel - 0) < 0.1:
-			steering_wheel = 0
-		elif steering_wheel > 0:
-			steering_wheel -= 0.1
-		elif steering_wheel < 0:
-			steering_wheel += 0.1
-	if is_pressed_space:
-		if gas < 1:
-			gas += 0.1
-		else:
-			gas = 1
-	else:
-		if gas > 0:
-			gas -= 0.1
-		else:
-			gas = 0
-	if is_pressed_shift:
-		if break_system < 1:
-			break_system += 0.1
-		else:
-			break_system = 1
-	else:
-		if break_system > 0:
-			break_system -= 0.1
-		else:
-			break_system = 0
+    if is_pressed_left ^ is_pressed_right:
+        if is_pressed_left:
+            if steering_wheel > -1:
+                steering_wheel -= 0.1
+            else:
+                steering_wheel = -1
+        if is_pressed_right:
+            if steering_wheel < 1:
+                steering_wheel += 0.1
+            else:
+                steering_wheel = 1
+    else:
+        if abs(steering_wheel - 0) < 0.1:
+            steering_wheel = 0
+        elif steering_wheel > 0:
+            steering_wheel -= 0.1
+        elif steering_wheel < 0:
+            steering_wheel += 0.1
+    if is_pressed_space:
+        if gas < 1:
+            gas += 0.1
+        else:
+            gas = 1
+    else:
+        if gas > 0:
+            gas -= 0.1
+        else:
+            gas = 0
+    if is_pressed_shift:
+        if break_system < 1:
+            break_system += 0.1
+        else:
+            break_system = 1
+    else:
+        if break_system > 0:
+            break_system -= 0.1
+        else:
+            break_system = 0
 
+if __name__ == '__main__':
+    env = gym.make('CarRacing-v0')
+    state = env.reset()
+    env.unwrapped.viewer.window.on_key_press = key_press
+    env.unwrapped.viewer.window.on_key_release = key_release
 
-env = gym.make('CarRacing-v0')
-observation = env.reset()
-env.unwrapped.viewer.window.on_key_press = key_press
-env.unwrapped.viewer.window.on_key_release = key_release
+    counter = 0
+    while not is_pressed_esc:
+        env.render()
+        update_action()
+        action = [steering_wheel, gas, break_system]
+        state, reward, done, info = env.step(action)
+        print('Action:[{:+.1f}, {:+.1f}, {:+.1f}] Reward: {:.3f}'.format(action[0], action[1], action[2], reward))
+        if done:
+            print("Episode finished after {0} timesteps".format(counter+1))
+            state = env.reset()
+            counter += 1
+            continue
 
-counter = 0
-while not is_pressed_esc:
-    env.render()
-    update_action()
-    action = [steering_wheel, gas, break_system]
-    observation, reward, done, info = env.step(action)
-    print('Action:[{:+.1f}, {:+.1f}, {:+.1f}] Reward: {:.3f}'.format(action[0], action[1], action[2], reward))
-    if done:
-        print("Episode finished after {0} timesteps".format(counter+1))
-        observation = env.reset()
-        counter += 1
-        continue
-
-env.close()
+    env.close()
