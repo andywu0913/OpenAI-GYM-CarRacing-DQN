7e394c860486c71adb37d9d4cfc9e261294df3c1
Andy Wu <andywu0913@yahoo.com.tw>
2020-04-28 00:52:25 +0800

    Using  to specify parameters when launching the program.

--- play_car_racing_by_the_model.py
+++ play_car_racing_by_the_model.py
@@ -1,11 +1,10 @@
+import argparse
 import cv2
 import gym
 import numpy as np
 from collections import deque
 from train_model import CarRacingDQNAgent
 
-
-EPISODES = 1
 
 def process_state_image(state):
     state = cv2.cvtColor(state, cv2.COLOR_BGR2GRAY)
@@ -18,13 +17,21 @@ def generate_state_frame_stack_from_queue(deque):
     # Move stack dimension to the channel dimension (stack, x, y) -> (x, y, stack)
     return np.transpose(frame_stack, (1, 2, 0))
 
+
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Play CarRacing by the trained model.')
+    parser.add_argument('-m', '--model', required=True, help='The `.h5` file of the trained model.')
+    parser.add_argument('-e', '--episodes', type=int, default=1, help='The number of episodes should the model plays.')
+    args = parser.parse_args()
+    train_model = args.model
+    play_episodes = args.episodes
+
     env = gym.make('CarRacing-v0')
     agent = CarRacingDQNAgent()
-    agent.load('./save/trial_250.h5')
+    agent.load(train_model)
     done = False
 
-    for e in range(EPISODES):
+    for e in range(play_episodes):
         init_state = env.reset()
         init_state = process_state_image(init_state)
         total_reward = 0
@@ -46,6 +53,6 @@ if __name__ == '__main__':
 
             if done:
                 agent.update_target_model()
-                print('Episode: {}/{}, Score(Time Frames): {}, Total Reward: {:.2}, Epsilon: {:.2}'.format(e+1, EPISODES, time_frame_counter, total_reward, agent.epsilon))
+                print('Episode: {}/{}, Score(Time Frames): {}, Total Reward: {:.2}, Epsilon: {:.2}'.format(e+1, play_episodes, time_frame_counter, total_reward, agent.epsilon))
                 break
             time_frame_counter += 1
